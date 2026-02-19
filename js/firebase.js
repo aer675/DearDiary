2d0abad2996f538eaf54cf8508ff0cd580e74e2a
lei-win <s10272765@connect.np.edu.sg>
2026-02-19 18:24:15 +0800

    Initializing API

--- js/firebase.js
+++ js/firebase.js
@@ -30,3 +30,65 @@ const auth = getAuth(app);
 window.auth = auth;
 window.createUserWithEmailAndPassword = createUserWithEmailAndPassword;
 window.signInWithEmailAndPassword = signInWithEmailAndPassword;
+
+// ============================
+// SIGN UP
+// ============================
+
+document.getElementById("signupForm").addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const email = document.getElementById("signupEmail").value;
+
+    const password = document.getElementById("signupPassword").value;
+
+
+    createUserWithEmailAndPassword(auth, email, password)
+
+    .then((userCredential) => {
+
+        alert("Account created!");
+
+    })
+
+    .catch((error) => {
+
+        alert(error.message);
+
+    });
+
+});
+
+
+
+// ============================
+// LOGIN
+// ============================
+
+document.getElementById("loginForm").addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const email = document.getElementById("loginEmail").value;
+
+    const password = document.getElementById("loginPassword").value;
+
+
+    signInWithEmailAndPassword(auth, email, password)
+
+    .then((userCredential) => {
+
+        alert("Welcome back Writer.");
+
+        window.location.href = "index.html";
+
+    })
+
+    .catch((error) => {
+
+        alert("Wrong email or password");
+
+    });
+
+});
